ff68bdce55334c9882f03a8cf50d9851d5cd7c8f
Tom Kwong <tomkwong@fb.com>
2021-03-21 18:50:27 -0700

    Clean up tz code and add 2 more reactors

--- src/command/tz.jl
+++ src/command/tz.jl
@@ -5,9 +5,14 @@ CITY_ALIASES = Dict(
     "Brussels" => "Europe/Brussels",
 )
 
+DATE_TIME_PATTERNS = (
+    dateformat"yyyy-mm-dd HH:MM",
+    dateformat"yyyy-mm-dd HH:MMp", # AM/PM
+)
+
 function time_zone_commander(c::Client, m::Message)
-    @info "time_zone_commander called"
-    @info "Message content" m.content m.author.username m.author.discriminator
+    # @info "time_zone_commander called"
+    # @info "Message content" m.content m.author.username m.author.discriminator
     startswith(m.content, COMMAND_PREFIX * "tz ") || return
     regex = Regex(COMMAND_PREFIX * raw"tz (help|convert|aliases) *(.*)$")
     matches = match(regex, m.content)
@@ -24,28 +29,29 @@ function time_zone_commander(c::Client, m::Message)
 end
 
 function help_time_zone_commander(c::Client, m::Message)
-    @info "Sending help for message" m.id m.author
+    # @info "Sending help for message" m.id m.author
     reply(c, m, """
         How to use the `tz` command:
         ```
         tz help
-        tz convert <date_time_spec>
+        tz convert <date> <time> <alias/timezone>
         tz aliases
         ```
-        The _date\\_time\\_spec_ may use city name aliases or any official time zone names from https://en.wikipedia.org/wiki/List_of_tz_database_time_zones:
+        Some examples are listed below.
+        You can use an alias or any official time zone names from https://en.wikipedia.org/wiki/List_of_tz_database_time_zones:
         ```
         2021-03-21 15:00 NY
-        2021-03-21 9:00 AM Rio
-        2021-03-21 9:00 AM America/Los_Angeles
+        2021-03-21 01:00AM Rio
+        2021-03-21 09:00PM America/Los_Angeles
         ```
         """)
 end
 
 function help_time_zone_aliases(c::Client, m::Message)
-    @info "Sending aliases for message" m.id m.author
+    # @info "Sending aliases for message" m.id m.author
     aliases = join(sort(["$(rpad(k, 12)) => $v" for (k,v) in CITY_ALIASES]), "\n")
     reply(c, m, """
-        tz command's city aliases are:
+        tz command's time zone aliases are:
         ```
         $aliases
         ```
@@ -65,7 +71,7 @@ function handle_time_zone_conversion(c::Client, m::Message, dts::AbstractString)
             """)
     catch ex
         @show ex
-        reply(c, m, "Sorry, I don't understand this date/time spec: $dts")
+        reply(c, m, "Sorry, I don't understand this format: `$dts`")
     end
 end
 
@@ -80,19 +86,16 @@ function parse_date_time_spec(dts::AbstractString)
 end
 
 function split_date_time_tokens(dts::AbstractString)
-    tokens = split(dts)
-    length(tokens) >= 3 || error("Bad date time spec: $dts")
+    tokens = split(dts, " ")
+    length(tokens) === 3 || error("Bad date time spec: $dts")
     return (
         datetime = join(tokens[1:2], " "),
-        city = join(tokens[3:end], "_")
+        city = tokens[3]
     )
 end
 
 function parse_date_time(dt::AbstractString)
-    for pattern in (
-        dateformat"yyyy-mm-dd HH:MM",
-        dateformat"yyyy-mm-dd HH:MMp", # AM/PM
-    )
+    for pattern in DATE_TIME_PATTERNS
         local datetime
         try
             return DateTime(dt, pattern)
@@ -108,9 +111,10 @@ function parse_time_zone(city::AbstractString)
 end
 
 function convert_time(t::ZonedDateTime)
+    display_format = "yyyy-mm-dd II:MMp"
     return [let
             datetime = astimezone(t, TimeZone(city))
-            display = Dates.format(datetime, "yyyy-mm-dd II:MM p") * " $alias"
+            display = Dates.format(datetime, display_format) * " $alias"
             (;alias, datetime, display)
         end for (alias, city) in CITY_ALIASES]
 end
