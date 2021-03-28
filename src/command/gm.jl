3a99f5d9bdae7ca0cd7af6940df78ba3b47adb38
Azzaare <jf@baffier.fr>
2021-03-28 12:30:13 +0900

    Basic help

--- src/command/gm.jl
+++ src/command/gm.jl
@@ -2,18 +2,30 @@ function game_master_commander(c::Client, m::Message)
     @info "game_master_commander called"
     startswith(m.content, COMMAND_PREFIX * "gm") || return
 
-    regex = Regex(COMMAND_PREFIX * raw"gm( help| opt-in| opt-out)? *(.*)$")
+    regex = Regex(COMMAND_PREFIX * raw"gm( help| in| out)? *(.*)$")
 
     matches = match(regex, m.content)
 
     if matches === nothing || matches.captures[1] ∈ (" help", nothing)
         help_game_master_commander(c, m)
-    elseif matches.captures[1] == "opt-in"
-        @info "Opt-in was required" m.content m.author.username m.author.discriminator
-    elseif matches.captures[1] == "opt-out"
-        @info "Opt-out was required" m.content m.author.username m.author.discriminator
+    elseif matches.captures[1] == "in"
+        @info "opt-in was required" m.content m.author.username m.author.discriminator
+    elseif matches.captures[1] == "out"
+        @info "opt-out was required" m.content m.author.username m.author.discriminator
     else
         reply(c, m, "Sorry, are you playing me? Please check out `gm help`")
     end
     return nothing
+end
+
+function help_game_master_commander(c::Client, m::Message)
+    reply(c, m, """
+        How to play with the `gm` command:
+        ```
+        gm help
+        gm in
+        gm out
+        ```
+        The commands `in` and `out` are to opt-in and opt-out of the game. Playing data of an `out` player are kept until the end of the game. An `out` player can come `in` anytime to resume its participation!
+        """)
 end
