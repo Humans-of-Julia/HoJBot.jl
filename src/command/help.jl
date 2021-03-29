4c641b1c9195ae666932fb2cb19fe12fe6a9da1e
Azzaare <jf@baffier.fr>
2021-03-29 22:03:39 +0900

    Fix help symbol

--- src/command/help.jl
+++ src/command/help.jl
@@ -1,15 +1,15 @@
-function commander(c::Client, m::Message, ::Val{:help})
+function commander(c::Client, m::Message, ::Val{:global_help})
     startswith(m.content, COMMAND_PREFIX * "help") || return
     regex = Regex(COMMAND_PREFIX * raw"help *(.*)$")
     matches = match(regex, m.content)
 
     if matches === nothing || matches.captures[1] ∈ (" help", nothing)
-        help_commander(c, m, :help)
+        help_commander(c, m, :global_help)
     end
     return nothing
 end
 
-function help_commander(c::Client, m::Message, ::Val{:help})
+function help_commander(c::Client, m::Message, ::Val{:global_help})
     commands = map(c -> string(c) * "\n", commands_list)
     opt = map(c -> string(c) * "\n", filter(c -> commands_list[c], opt_services_list))
     reply(c, m,
