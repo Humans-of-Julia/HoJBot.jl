caa4e2aabd399678a764656d46f29faac587d656
Azzaare <jf@baffier.fr>
2021-03-29 17:10:18 +0900

    Commented the gm command

--- src/main.jl
+++ src/main.jl
@@ -15,7 +15,7 @@ end
 function init_commands!(client::Client)
     add_command!(client, :tz, time_zone_commander)
     add_command!(client, :j, julia_commander)
-    add_command!(client, :gm, game_master_commander)
+    # add_command!(client, :gm, game_master_commander)
     add_command!(client, :react, reaction_commander)
 end
 
