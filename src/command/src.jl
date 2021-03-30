39a1f664e56ba0e8d50bcba9492a2c7b92f2fa93
Azzaare <jf@baffier.fr>
2021-03-30 11:40:48 +0900

    Change source to src

--- src/command/src.jl
+++ src/command/src.jl
@@ -6,8 +6,8 @@ const sources_url = LittleDict([
 Returns the url of the source repo of HojBot.
 """
 function commander(c::Client, m::Message, ::Val{:source})
-    startswith(m.content, COMMAND_PREFIX * "source") ||  return
-    regex = Regex(COMMAND_PREFIX * raw"source (repo|repository|help)$") # will add something soon to return specific code of commands
+    startswith(m.content, COMMAND_PREFIX * "src") ||  return
+    regex = Regex(COMMAND_PREFIX * raw"src (repo|repository|help) *(.*)$") # will add something soon to return specific code of commands
     matches = match(regex, m.content)
     if matches === nothing || matches.captures[1] == "repo" || matches.captures[1] == "repository"
         reply(c, m, sources_url["HoJBot"])
