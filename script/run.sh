2f186aa728a1cd5f714d041bbcbf556583fbd065
Tom Kwong <tk3369@gmail.com>
2021-04-15 00:08:34 -0700

    Auto shutdown and restart bot periodically

--- script/run.sh
+++ script/run.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
+# Start the HoJBot program
 
 if [[ -z "$HOJBOT_DISCORD_TOKEN" ]]; then
     echo "Error: please set up HOJBOT_DISCORD_TOKEN environment variable"
     exit 1
 fi
 
-julia --project=. -e 'using HoJBot; start_bot();'
+# Default environment settings
+: ${RUN_ONCE=no}
+: ${RUN_DURATION_MINUTES=360}
+
+while true; do
+    echo "`date`: Starting HoJBot..."
+    julia --project=. -e "using HoJBot, Dates; start_bot(; run_duration = Minute(${RUN_DURATION_MINUTES}));"
+    if [[ "$RUN_ONCE" == "yes" ]]; then
+        break
+    fi
+    sleep 1  # throttle to avoid a hot loop when the process keep crashing
+done
+echo "`date`: Program exited normally"
