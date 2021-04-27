59942e737ab415bdf743d7e6c6e8b29b361e1457
Soc Virnyl S. Estela <66054069+ReneganRonin@users.noreply.github.com>
2021-04-27 14:51:30 +0800

    Include constants file

--- src/HoJBot.jl
+++ src/HoJBot.jl
@@ -22,6 +22,7 @@ import StructTypes
 
 const COMMAND_PREFIX = get(ENV, "HOJBOT_COMMAND_PREFIX", ",")
 
+include("constants.jl")
 include("dispatcher.jl")
 include("util.jl")
 include("discord.jl")
@@ -31,6 +32,7 @@ include("command/tz.jl")
 include("command/j.jl")
 include("command/gm.jl")
 include("command/react.jl")
+include("command/src.jl")
 
 include("handler/reaction.jl")
 include("handler/whistle.jl")
