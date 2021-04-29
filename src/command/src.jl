5dc72cf2c11e912b86fcede1a1026f0c7f532ee3
Soc Virnyl S. Estela <66054069+ReneganRonin@users.noreply.github.com>
2021-04-29 09:21:17 +0800

    Refactor source command

--- src/command/src.jl
+++ src/command/src.jl
@@ -1,7 +1,8 @@
+const filtered_active_cmds = keys((filter(check -> last(check) == true, active_commands)))
+
 function commander(c::Client, m::Message, ::Val{:source})
     startswith(m.content, COMMAND_PREFIX * "src") || return
 
-    # regex = Regex(COMMAND_PREFIX * raw"src( help| discourse| gm| ig| j| react| src| tz)? *(.*)$")
     regex = Regex(COMMAND_PREFIX * raw"src( help| [a-zA-Z]*)? *(.*)$")
 
     matches = match(regex, m.content)
@@ -10,8 +11,7 @@ function commander(c::Client, m::Message, ::Val{:source})
         help_commander(c, m, :source)
     else
         command = lowercase(strip(matches.captures[1]))
-        real_active_cmds = keys((filter(check -> last(check) == true, active_commands)))
-        if Symbol(command) ∈ real_active_cmds
+        if Symbol(command) ∈ filtered_active_cmds
             msg = string(BOT_REPO_URL, "/blob/main/src/command/", command, ".jl")
             reply(c, m, msg)
         else
@@ -22,15 +22,17 @@ function commander(c::Client, m::Message, ::Val{:source})
 end
 
 function help_commander(c::Client, m::Message, ::Val{:source})
+    list_commands =
+    join(sort([string("src", " ", cmd, "\n") for cmd in filtered_active_cmds]))
     return reply(
         c, m, """
-     How to use `src` command:
+     Returns the source file of the command.
+     Usage: `src <command>`
+     List of available commands:
      ```
-     src help
-     src react
-     src <command>
+     $(list_commands)
      ```
-     Check out the $(BOT_REPO_URL) to see the code and commands.
+     `src` or `src help` returns help
      """
     )
 end
