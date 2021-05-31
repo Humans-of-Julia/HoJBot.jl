8a8617064762da0948a6e80322d98073d1bc10a0
Ricardo Rosa <rmsrosa@gmail.com>
2021-05-31 08:37:10 -0300

    fix docstring for struct and possibly others

--- src/command/j.jl
+++ src/command/j.jl
@@ -1,47 +1,4 @@
-# implement julia_commander
-# for the moment, only help is implemented
-
-function filterranges(u::Vector{UnitRange{Int}})
-    v = Vector{Int}()
-    while length(u) > 1
-        if all(m -> (u[1] ⊈ m), u[2:end])
-            push!(v, u[1][end])
-        end
-        u = u[findall(m -> m ⊈ u[1], u[2:end]).+1]
-    end
-    if length(u) == 1
-        push!(v,u[1][end])
-    end
-    return v
-end
-
-const STYLES = [
-    r"```.+?```"s, r"`.+?`", r"~~.+?~~", r"_.+?_", r"__.+?__",
-    r"\*.+?\*", r"\*\*.+?\*\*", r"\*\*\*.+?\*\*\*",
-]
-
-function split_message_fixed(text::AbstractString, chunk_limit::Int=2000; extraregex::Vector{Regex}=Vector{Regex}())
-    length(text) <= chunk_limit && return String[text]
-    chunks = String[]
-
-    while !isempty(text)
-        mranges = vcat(findall.(union(STYLES,extraregex),Ref(text))...)
-
-        stop = maximum(filter(i -> length(text[1:i]) ≤ chunk_limit, filterranges(mranges)))
-
-        # give up if first chunk cannot be broken down
-        if stop == 0
-            push!(chunks, strip(text))
-            @warn "message could not be broken down into chunks smaller than the desired length $chunk_limit"
-            return chunks
-        end
-
-        push!(chunks, strip(text[1:stop]))
-        text = strip(text[stop+1:end])
-    end
-
-    return chunks
-end
+# implement julia_commander for showing docstrings
 
 function parse_doc(doc::AbstractString)
     doc = replace(doc, r"\n\n\n+" => "\n\n")
@@ -111,7 +68,6 @@ function handle_julia_help_commander(c::Client, m::Message, name)
     try
         doc = string(eval(:(Base.Docs.@doc $(Symbol(name)))))
         doc = parse_doc(doc)
-        user = @discord retrieve(c, User, m.author.id)
         channel = @discord get_channel(c, m.channel_id)
         if !occursin("No documentation found", doc)
             count = update_names_count(
@@ -119,9 +75,13 @@ function handle_julia_help_commander(c::Client, m::Message, name)
                 m.channel_id, channel.name)
             doc *= "\n*(Count number for `$name`: $count)*"
         end
-        docs = split_message_fixed(doc, extraregex = [r"\n≡.+\n", r"n-.+\n"])
-        for doc_chunck in docs
-            reply(c, m, doc_chunck)
+        if startswith(doc, r"```(?!julia)")
+            doc = replace(doc, "```" => "```\n", count=1)
+        end
+        docs = split_message(doc, extrastyles = [r"\n≡.+\n", r"n-.+\n"])
+        for doc_chunk in docs
+            # @show doc_chunk
+            reply(c, m, doc_chunk)
         end
     catch ex
         @show ex
@@ -133,13 +93,13 @@ end
 function handle_doc_stats(c::Client, m::Message, captured::AbstractString)
     # @info "handle_doc_stats called"
     try
-        r = match(r"(top|head|high|bottom|tail|low) +(\d*) *$", captured)
+        r = match(r"(top|head|bottom|tail) +(\d*) *$", captured)
         if captured == ""
             statsmgs = stats_namescount("namescount")
         elseif r === nothing
             statsmgs = stats_namescount("namescount", name=strip(captured))        
         else
-            place = r.captures[1] in ("top", "head", "high") ? "top" : "bottom"
+            place = r.captures[1] in ("top", "head") ? "top" : "bottom"
             statsmgs = stats_namescount("namescount", place=place, number=parse(Int,r.captures[2]))
         end
         reply(c, m, statsmgs)
