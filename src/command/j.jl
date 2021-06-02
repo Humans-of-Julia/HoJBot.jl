894e8618d5ca18307b27ee3591a9408c5fef1998
Ricardo Rosa <rmsrosa@gmail.com>
2021-06-02 16:37:40 -0300

    make `j top` and `j bottom` apart from `j stats`

--- src/command/j.jl
+++ src/command/j.jl
@@ -26,7 +26,7 @@ function commander(c::Client, m::Message, ::Val{:julia_doc})
     # @info "julia_commander called"
     # @info "Message content" m.content m.author.username m.author.discriminator
     startswith(m.content, COMMAND_PREFIX * "j") || return
-    regex = Regex(COMMAND_PREFIX * raw"j(\?| help| doc| packages| package| stats)? *(.*)$")
+    regex = Regex(COMMAND_PREFIX * raw"j(\?| help| doc| packages| package| stats| top| bottom)? *(.*)$")
     matches = match(regex, m.content)
     if matches === nothing || matches.captures[1] in (" help", nothing)
         help_commander(c, m, Val(:julia_doc))
@@ -36,8 +36,8 @@ function commander(c::Client, m::Message, ::Val{:julia_doc})
         handle_julia_package_list(c, m)
     elseif matches.captures[1] == " package"
         handle_julia_package_names(c, m, matches.captures[2])
-    elseif matches.captures[1] == " stats"
-        handle_doc_stats(c, m, matches.captures[2])
+    elseif matches.captures[1] in (" stats", " top", " bottom")
+        handle_doc_stats(c, m, matches.captures[1], matches.captures[2])
     else
         reply(c, m, "Sorry, I don't understand the request; use `j help` for help")
     end
@@ -47,25 +47,30 @@ end
 function help_commander(c::Client, m::Message, ::Val{:julia_doc})
     # @info "Sending help for message" m.id m.author
     reply(c, m, """
-        The `j` commands shows the docstring of keywords, names in Base and names in other selected packages.
-        It also gives as statistics of its use.
+        The `j` command shows the docstring of keywords, names in Base and names in other selected packages. Only names with meaningful help information have been recorded from each package.
+        
+        The `j` command also gives statistics on the names that have been queried, via `j stats`.
 
-        Here are the available `j` commands and their use:
+        To see which packages have been added, use `j packages`. If you would like a particular package to be added, just let us know.
+
+        Here is the list of all available `j` commands and their use:
         ```
         j help
         j? <name>
         j doc <name>
         j packages
         j package <Name>
-        j stats <name>
-        j stats <place> <number>
+        j stats [<name>]
+        j top [<number>]
+        j bottom [<number>]
         ```
         * `j help` returns this help.
         * `j packages` shows which packages are available with names.
         * `j package <PkgName>` shows which names have bee recorded from to package <PkgName>.
         * `j? <name>` and `j doc <name>` return the documentation for `<name>`
         * `j stats <name>` return how many times the docstring for `name` has been queried.
-        * `j stats <place> <number>` return the top (if `place` is equal to either "top" or "head") or the bottom (if `place` is equal to either "bottom" or "tail") `number` names that have been queried.
+        * `j top [<number>]` return the top 10 names that have been queried (or top `number`, if given).
+        * `j bottom [<number>]` return the bottom 10 names that have been queried (or bottom `number`, if given).
         """)
     return nothing
 end
@@ -95,9 +100,9 @@ function handle_julia_help_commander(c::Client, m::Message, name)
                         doc = "*In Package* `$k`:\n" * v
                     end
                     doc = parse_doc(doc)
-                    docs = split_message(doc, extrastyles = [r"\n.*\n≡.+\n", r"\n.*\n-+\n"])
+                    docs = split_message(doc, extrastyles = [r"\n.*\n≡.+\n", r"\n.*\n-+\n", r"[^\s]+"])
                     for doc_chunk in docs
-                        # @show doc_chunk
+                        # @info doc_chunk
                         reply(c, m, doc_chunk)
                     end
                 end
@@ -113,27 +118,33 @@ function handle_julia_help_commander(c::Client, m::Message, name)
             reply(c, m, "No documentation found.")
         end
     catch ex
-        @show ex
+        @info ex
         reply(c, m, "Sorry, it didn't work.")
     end
     return nothing
 end
 
-function handle_doc_stats(c::Client, m::Message, captured::AbstractString)
+function handle_doc_stats(c::Client, m::Message, captured1::AbstractString, captured2::AbstractString)
     # @info "handle_doc_stats called"
+    captured2 = strip(captured2)
+    captured2 = replace(captured2, r"\s{2,}" => " ")
     try
-        r = match(r"(top|head|bottom|tail) +(\d*) *$", captured)
-        if captured == ""
-            statsmgs = stats_namescount("namescount")
-        elseif r === nothing
-            statsmgs = stats_namescount("namescount", name=strip(captured))        
-        else
-            place = r.captures[1] in ("top", "head") ? "top" : "bottom"
-            statsmgs = stats_namescount("namescount", place=place, number=parse(Int,r.captures[2]))
+        if captured1 == " stats"
+            if captured2 == ""
+                statsmgs = stats_namescount("namescount")
+            else
+                statsmgs = stats_namescount("namescount", name=captured2)
+            end
+        elseif captured1 in (" top", " bottom")
+            if captured2 != "" && all(isdigit,captured2) 
+                statsmgs = stats_namescount("namescount", place=strip(captured1), number=max(1, parse(Int,captured2)))
+            else
+                statsmgs = stats_namescount("namescount", place=strip(captured1))
+            end
         end
         reply(c, m, statsmgs)
     catch ex
-        @show ex
+        @info ex
         reply(c, m, "Sorry, it didn't work.")
     end
     return nothing
@@ -155,7 +166,7 @@ end
 function handle_julia_package_names(c::Client, m::Message, pkg)
     pkg = strip(pkg)
     pkg = replace(pkg, r"\s{2,}" => " ")
-    @info pkg
+    # @info pkg
     all_names_filename = joinpath(@__DIR__, "..", "..", "data", "docs", "all_names.json")
     if isfile(all_names_filename)
         all_names = load_names(all_names_filename)
